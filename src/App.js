0c8956e1efbc32c4126a87922d8e394d9a385ac1
mamun07 <mamunben7@gmail.com>
2022-06-19 13:56:19 +0600

    form page create done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { Button } from 'antd';
+import { Layout } from 'antd';
+import { Col, Row } from 'antd';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+const { Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <h2>Welcome to React TO DO LIST</h2>
-      <Button type="primary">Primary Button</Button>
-    </div>
+    <Layout className="main-container">
+      <Content>
+        <Row>
+          <Col span={12} offset={6}>
+            <TodoForm />
+            <TodoList />
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
   );
 }
 
