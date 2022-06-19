66fea154525f037215c0eb31a98ee5065e75aa39
mamun07 <mamunben7@gmail.com>
2022-06-19 13:40:10 +0600

    React to do list using react & zustand

---
+++ src/store/todolistStore.js
@@ -0,0 +1,31 @@
+import create from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+
+const todolistStore = (set) => ({
+  todolists: [],
+  addTodolist: (todolist) => {
+    set((state) => ({
+      todolists: [todolist, ...state.todolists],
+    }));
+  },
+  removeTodolist: (todolistId) => {
+    set((state) => ({
+      todolists: state.todolists.filter((todo) => todo.id !== todolistId),
+    }));
+  },
+  toggleTodolistStatus: (todolistId) => {
+    set((state) => ({
+      todolists: state.todolists.map((todolist) =>
+        todolist.id === todolistId
+          ? { ...todolist, completed: !todolist.completed }
+          : todolist
+      ),
+    }));
+  },
+});
+
+const useTodolistStore = create(
+  devtools(persist(todolistStore, { name: 'todolists' }))
+);
+
+export default useTodolistStore;
